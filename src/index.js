5c41ed10f9974d0898c7c3ba8f11f558b77be0f2
Richard Chen <richard.c.chen@gmail.com>
2019-02-19 16:24:58 -0500

    added component cards

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App  = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45pm" commentText="Awesome" image={faker.image.avatar()}/>
-      <CommentDetail author="Alex" timeAgo="Today at 6:00pm" commentText="Cool" image={faker.image.avatar()}/>
-      <CommentDetail author="Jane" timeAgo="Yesterdat at 3:45pm" commentText="Needs more information!" image={faker.image.avatar()}/>
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 4:45pm" commentText="Awesome" image={faker.image.avatar()}/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today at 6:00pm" commentText="Cool" image={faker.image.avatar()}/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Jane" timeAgo="Yesterdat at 3:45pm" commentText="Needs more information!" image={faker.image.avatar()}/>
+      </ApprovalCard>
     </div>
 
   )
